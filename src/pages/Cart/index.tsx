ca74acc1c5044ad0adddb4402a64c1087002a940
Davi Lima <davi.rlima04@gmail.com>
2022-09-20 22:22:33 -0400

    Criação do frame complete seu pedido

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,14 +1,80 @@
-import { CartContainer, Frame } from "./styles";
+import {
+  ButtonsPaymentContainer,
+  CartContainer,
+  FormAddressContainer,
+  Frame,
+  FrameContentContainer,
+} from "./styles";
+import {
+  MapPinLine,
+  CurrencyDollar,
+  CreditCard,
+  Bank,
+  Money,
+} from "phosphor-react";
 
 export function Cart() {
   return (
     <CartContainer>
       <Frame>
         <h1>Complete seu pedido</h1>
-        <div>
-          <div></div>
-          <div></div>
-        </div>
+
+        <FrameContentContainer>
+          <div className="header">
+            <MapPinLine size={22} className="icon-yellow_dark" />
+            <div>
+              <h2>Endereço de entrega</h2>
+              <p>Informe o endereço onde deseja receber seu pedido</p>
+            </div>
+          </div>
+
+          <FormAddressContainer action="">
+            <input type="text" placeholder="CEP" id="cep" />
+            <input type="text" placeholder="Rua" />
+            <div>
+              <input type="text" placeholder="Número" id="numero" />
+              <input type="text" placeholder="Complemento" id="complemento" />
+            </div>
+            <div>
+              <input type="text" placeholder="Bairro" id="bairro" />
+              <input type="text" placeholder="Cidade" id="cidade" />
+              <input type="text" placeholder="UF" id="uf" />
+            </div>
+          </FormAddressContainer>
+        </FrameContentContainer>
+
+        <FrameContentContainer>
+          <div className="header">
+            <CurrencyDollar size={22} className="icon-purple" />
+            <div>
+              <h2>Pagamento</h2>
+              <p>
+                O pagamento é feito na entrega. Escolha a forma que deseja pagar
+              </p>
+            </div>
+          </div>
+
+          <ButtonsPaymentContainer>
+            <div>
+              <input type="radio" id="credit" name="payment" />
+              <label htmlFor="credit">
+                <CreditCard className="icon" /> CARTÃO DE CRÉDITO
+              </label>
+            </div>
+            <div>
+              <input type="radio" id="debit" name="payment" />
+              <label htmlFor="debit">
+                <Bank className="icon" /> CARTÃO DE DÉBITO
+              </label>
+            </div>
+            <div>
+              <input type="radio" id="money" name="payment" />
+              <label htmlFor="money">
+                <Money className="icon" /> DINHEIRO
+              </label>
+            </div>
+          </ButtonsPaymentContainer>
+        </FrameContentContainer>
       </Frame>
       <Frame>
         <h1>Cafés selecionados</h1>
--- src/pages/Cart/styles.ts
+++ src/pages/Cart/styles.ts
@@ -22,11 +22,125 @@ export const CartContainer = styled.main`
 `;
 
 export const Frame = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
   width: 100%;
 
   h1 {
     font-size: 1.125rem;
     color: ${(props) => props.theme.base_subtitle};
     margin-bottom: 1rem;
+  }
+`;
+
+export const FrameContentContainer = styled.div`
+  padding: 2.5rem;
+
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+
+  background-color: ${(props) => props.theme.base_card};
+  border-radius: 6px;
+
+  .header {
+    display: flex;
+    gap: 0.5rem;
+    align-items: flex-start;
+
+    .icon-yellow_dark {
+      color: ${(props) => props.theme.yellow_dark};
+    }
+    .icon-purple {
+      color: ${(props) => props.theme.purple};
+    }
+
+    h2 {
+      font-size: 1rem;
+      font-weight: 400;
+      color: ${(props) => props.theme.base_subtitle};
+    }
+    p {
+      font-size: 0.875rem;
+    }
+  }
+`;
+
+export const FormAddressContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  div {
+    display: flex;
+    gap: 0.75rem;
+  }
+
+  input {
+    padding: 0.75rem;
+    border-radius: 4px;
+    background-color: ${(props) => props.theme.base_input};
+    border: 1px solid ${(props) => props.theme.base_button};
+
+    font-size: 0.875rem;
+    color: ${(props) => props.theme.base_txt};
+
+    ::placeholder {
+      color: ${(props) => props.theme.base_label};
+    }
+
+    :focus {
+      outline: none;
+      border: 1px solid ${(props) => props.theme.yellow_dark};
+    }
+  }
+
+  #cep,
+  #numero,
+  #bairro {
+    width: 12.5rem;
+  }
+  #complemento {
+    width: 21.75rem;
+  }
+  #cidade {
+    width: 17.25rem;
+  }
+  #uf {
+    width: 3.75rem;
+  }
+`;
+
+export const ButtonsPaymentContainer = styled.div`
+  display: flex;
+  gap: 0.75rem;
+
+  input {
+    display: none;
+    :checked {
+      ~ label {
+        background-color: ${(props) => props.theme.purple_light};
+        border: 1px solid ${(props) => props.theme.purple};
+      }
+    }
+  }
+
+  label {
+    position: relative;
+    display: flex;
+    align-items: center;
+    gap: 0.75rem;
+
+    padding: 1rem;
+    background-color: ${(props) => props.theme.base_button};
+    border: 1px solid transparent;
+    border-radius: 6px;
+    font-size: 0.75rem;
+
+    .icon {
+      font-size: 1rem;
+      color: ${(props) => props.theme.purple};
+    }
   }
 `;
